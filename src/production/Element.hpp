b4b375c5c23375a7208dd70a44b322e925cf417b
NiclasDimitriadis <n.dimitriadis@protonmail.com>
2025-05-29 18:45:26 +0200

    bug fix. copying now properly atomic when reading from queue

--- src/production/Element.hpp
+++ src/production/Element.hpp
@@ -5,67 +5,67 @@
 #include <optional>
 #include <tuple>
 
-
 #include "SLQ_Auxil.hpp"
 
 namespace Element {
-template <typename ContentType, std::uint32_t alignment>
+template<typename ContentType, std::uint32_t alignment>
 struct alignas(alignment) SeqLockElement {
-  using PayloadType = ContentType::type;
-  ContentType content;
-  std::atomic<std::int64_t> version = 0;
-  void insert(const PayloadType&) noexcept;
-  std::tuple<std::optional<PayloadType>, std::int64_t>
-  read(const std::int64_t) const noexcept;
-  explicit SeqLockElement() noexcept = default;
-  ~SeqLockElement() = default;
-  SeqLockElement(const SeqLockElement&) = delete;
-  // copy assigment operator needs to be defined
-  SeqLockElement& operator=(const SeqLockElement&) noexcept;
-  SeqLockElement(SeqLockElement&&) = delete;
-  SeqLockElement& operator=(SeqLockElement&&) = delete;
+   using PayloadType = ContentType::type;
+   ContentType content;
+   std::atomic<std::int64_t> version = 0;
+   void insert(const PayloadType&) noexcept;
+   std::tuple<std::optional<PayloadType>, std::int64_t>
+      read(const std::int64_t) const noexcept;
+   explicit SeqLockElement() noexcept = default;
+   ~SeqLockElement() = default;
+   SeqLockElement(const SeqLockElement&) = delete;
+   // copy assigment operator needs to be defined
+   SeqLockElement& operator=(const SeqLockElement&) noexcept;
+   SeqLockElement(SeqLockElement&&) = delete;
+   SeqLockElement& operator=(SeqLockElement&&) = delete;
 };
 };
 
-#define TEMPLATE_PARAMS                                                        \
-  template <typename ContentType, std::uint32_t alignment>
+#define TEMPLATE_PARAMS \
+   template<typename ContentType, std::uint32_t alignment>
 
 #define SEQ_LOCK_ELEMENT Element::SeqLockElement<ContentType, alignment>
 
 TEMPLATE_PARAMS
 void SEQ_LOCK_ELEMENT::insert(const PayloadType& new_content) noexcept {
-  this->version.fetch_add(1,std::memory_order_acquire);
-  this->content = ContentType(new_content);
-  this->version.fetch_add(1,std::memory_order_release);
+   this->version.fetch_add(1, std::memory_order_acquire);
+   this->content = ContentType(new_content);
+   this->version.fetch_add(1, std::memory_order_release);
 };
 
 TEMPLATE_PARAMS
 std::tuple<std::optional<typename SEQ_LOCK_ELEMENT::PayloadType>, std::int64_t>
-SEQ_LOCK_ELEMENT::read(const std::int64_t prev_version) const noexcept {
-  std::tuple<std::optional<PayloadType>, std::int64_t> ret;
-  std::optional<PayloadType>& ret_opt = std::get<0>(ret);
-  std::int64_t& initial_version = std::get<1>(ret);
-  std::atomic<std::int64_t> final_version;
-  do {
-    initial_version = this->version.load(std::memory_order_acquire);
-    // atomic byte wise copying if accept_UB == false
-    // make use of implicit conversion
-    ret_opt = this->content;
-    final_version.store(this->version,std::memory_order_release);
-    // spin if write took place while reading
-  } while (initial_version % 2 || (initial_version != final_version));
-  ret_opt = initial_version >= prev_version ? ret_opt : std::nullopt;
-  return ret;
+   SEQ_LOCK_ELEMENT::read(const std::int64_t prev_version) const noexcept {
+   // first element of ret will be implicitly converted to std::optional<PayloadType> when returned
+   std::tuple<std::optional<ContentType>, std::int64_t> ret;
+   std::optional<ContentType>& ret_opt = std::get<0>(ret);
+   std::int64_t& initial_version = std::get<1>(ret);
+   std::atomic<std::int64_t> final_version;
+   do {
+      initial_version = this->version.load(std::memory_order_acquire);
+      // atomic byte wise copying if accept_UB == false
+      ret_opt = this->content;
+      final_version.store(this->version, std::memory_order_release);
+      // spin if write took place while reading
+   }
+   while(initial_version % 2 || (initial_version != final_version));
+   ret_opt = initial_version >= prev_version ? ret_opt : std::nullopt;
+   return ret;
 };
 
 TEMPLATE_PARAMS
-SEQ_LOCK_ELEMENT& SEQ_LOCK_ELEMENT::operator=(const SEQ_LOCK_ELEMENT& other) noexcept{
-  // read() provides functionality to safely retrieve content and version
-  // read() always returns content if called with version argument 0
-  const auto read_ret = other.read(0);
-  this->content = ContentType(std::get<0>(read_ret).value());
-  this->version.store(std::get<1>(read_ret), std::memory_order_relaxed);
-  return *this;
+SEQ_LOCK_ELEMENT& SEQ_LOCK_ELEMENT::operator=(const SEQ_LOCK_ELEMENT& other) noexcept {
+   // read() provides functionality to safely retrieve content and version
+   // read() always returns content if called with version argument 0
+   const auto read_ret = other.read(0);
+   this->content = ContentType(std::get<0>(read_ret).value());
+   this->version.store(std::get<1>(read_ret), std::memory_order_relaxed);
+   return *this;
 };
 
 #undef TEMPLATE_PARAMS
--- src/production/SLQ_Auxil.hpp
+++ src/production/SLQ_Auxil.hpp
@@ -6,133 +6,141 @@
 #include <type_traits>
 #include <utility>
 
-
 namespace SLQ_Auxil {
-template <typename I>
-  requires std::unsigned_integral<I>
+template<typename I>
+requires std::unsigned_integral<I>
 constexpr I ceil_(I i1, I i2) {
-  if (i1 % i2 == 0)
-    return i1;
-  else
-    return ceil_(static_cast<I>(i1 + 1), i2);
+   if(i1 % i2 == 0) {
+      return i1;
+   }
+   else {
+      return ceil_(static_cast<I>(i1 + 1), i2);
+   }
 };
 
-template <typename I>
-  requires std::unsigned_integral<I>
+template<typename I>
+requires std::unsigned_integral<I>
 constexpr I divisible_(I i1, I i2) {
-  if (i2 % i1 == 0)
-    return i1;
-  else
-    return divisible_(static_cast<I>(i1 + 1), i2);
+   if(i2 % i1 == 0) {
+      return i1;
+   }
+   else {
+      return divisible_(static_cast<I>(i1 + 1), i2);
+   }
 };
 
-template <typename I>
-  requires std::unsigned_integral<I>
+template<typename I>
+requires std::unsigned_integral<I>
 constexpr I divisible_or_ceil(I i1, I i2) {
-  if (i1 < i2)
-    return divisible_(i1, i2);
-  else
-    return ceil_(i1, i2);
+   if(i1 < i2) {
+      return divisible_(i1, i2);
+   }
+   else {
+      return ceil_(i1, i2);
+   }
 };
-
 
 template<typename...>
-struct atomic_arr_copy{
-  static_assert(false);
+struct atomic_arr_copy {
+   static_assert(false);
 };
 
 template<typename T, size_t... indices>
 requires std::is_trivially_copyable_v<T>
-&& std::is_default_constructible_v<T>
-&& std::atomic<char>::is_always_lock_free
-struct atomic_arr_copy<T, std::integer_sequence<size_t, indices...>>{
+   && std::is_default_constructible_v<T>
+   && std::atomic<char>::is_always_lock_free
+struct atomic_arr_copy<T, std::integer_sequence<size_t, indices...>> {
 private:
-  static constexpr size_t size = sizeof(T);
-  using SrcSpanType = const std::span<const std::atomic<char>, size>;
-  using DestSpanType = std::span<std::atomic<char>, size>;
-  T value;
+   static constexpr size_t size = sizeof(T);
+   using SrcSpanType = const std::span<const char, size>;
+   using DestSpanType = std::span<std::atomic<char>, size>;
+   T value;
+
 public:
-  using type = T;
+   using type = T;
 
-  // allow nothrow conversion to T
-  operator const T&() const noexcept{return value;};
+   // allow nothrow conversion to T
+   operator const T&() const noexcept { return value; };
 
-  explicit atomic_arr_copy(){};
+   explicit atomic_arr_copy() {};
 
-  atomic_arr_copy(const T arg): value{arg}{};
+   atomic_arr_copy(const T arg):
+       value{arg} {};
 
-  atomic_arr_copy& operator=(const atomic_arr_copy& other){
-    // make relevant sections of memory accessible via std::span
-    const auto source = SrcSpanType{reinterpret_cast<const std::atomic<char>*>(&other.value), size};
-    auto dest = DestSpanType{reinterpret_cast<std::atomic<char>*>(&this->value), size};
-    // atomically copy data
-    (dest[indices].store(source[indices], std::memory_order_relaxed),...);
-    return *this;
-  };
+   atomic_arr_copy& operator=(const atomic_arr_copy& other) {
+      // make relevant sections of memory accessible via std::span
+      const auto source = SrcSpanType{reinterpret_cast<const char*>(&other.value), size};
+      auto dest = DestSpanType{reinterpret_cast<std::atomic<char>*>(&this->value), size};
+      // atomically copy data
+      (dest[indices].store(source[indices], std::memory_order_relaxed), ...);
+      return *this;
+   };
 
-  atomic_arr_copy(const atomic_arr_copy& other){
-    *this = other;
-  };
+   atomic_arr_copy(const atomic_arr_copy& other) {
+      *this = other;
+   };
 
-  atomic_arr_copy& operator=(const atomic_arr_copy&& other){
-    return operator=(other);
-  };
+   atomic_arr_copy& operator=(const atomic_arr_copy&& other) {
+      return operator=(other);
+   };
 
-  atomic_arr_copy(const atomic_arr_copy&& other){
-    *this = other;
-  };
+   atomic_arr_copy(const atomic_arr_copy&& other) {
+      *this = other;
+   };
 };
 
-template <typename T>
+template<typename T>
 using atomic_arr_copy_t = atomic_arr_copy<T, std::make_index_sequence<sizeof(T)>>;
 
-template <typename T>
+template<typename T>
 requires std::is_default_constructible_v<T>
-&& std::is_copy_assignable_v<T>
-struct atomic_arr_copy_standin{
+   && std::is_copy_assignable_v<T>
+struct atomic_arr_copy_standin {
 private:
-  T value;
+   T value;
+
 public:
-  using type = T;
+   using type = T;
 
-  // allow nothrow conversion to T
-  operator const T&() const noexcept{return value;};
+   // allow nothrow conversion to T
+   operator const T&() const noexcept { return value; };
 
-  explicit atomic_arr_copy_standin(){};
+   explicit atomic_arr_copy_standin() {};
 
-  atomic_arr_copy_standin(const T& arg): value(arg){};
+   atomic_arr_copy_standin(const T& arg):
+       value(arg) {};
 
-  atomic_arr_copy_standin& operator=(const atomic_arr_copy_standin& other){
-    this->value = other.value;
-    return *this;
-  };
+   atomic_arr_copy_standin& operator=(const atomic_arr_copy_standin& other) {
+      this->value = other.value;
+      return *this;
+   };
 
-  atomic_arr_copy_standin(const atomic_arr_copy_standin& other){
-    *this = other;
-  };
+   atomic_arr_copy_standin(const atomic_arr_copy_standin& other) {
+      *this = other;
+   };
 
-  atomic_arr_copy_standin& operator=(const atomic_arr_copy_standin&& other){
-    return operator=(other);
-  };
+   atomic_arr_copy_standin& operator=(const atomic_arr_copy_standin&& other) {
+      return operator=(other);
+   };
 
-  atomic_arr_copy_standin(const atomic_arr_copy_standin&& other){
-    *this = other;
-  };
+   atomic_arr_copy_standin(const atomic_arr_copy_standin&& other) {
+      *this = other;
+   };
 };
 
 template<typename, bool>
-struct UB_or_not_UB_logic{
-    static_assert(false);
+struct UB_or_not_UB_logic {
+   static_assert(false);
 };
 
 template<typename T>
-struct UB_or_not_UB_logic<T, true>{
-  using type = atomic_arr_copy_standin<T>;
+struct UB_or_not_UB_logic<T, true> {
+   using type = atomic_arr_copy_standin<T>;
 };
 
 template<typename T>
-struct UB_or_not_UB_logic<T, false>{
-  using type = atomic_arr_copy_t<T>;
+struct UB_or_not_UB_logic<T, false> {
+   using type = atomic_arr_copy_t<T>;
 };
 
 template<typename T, bool accept_UB>
